73e1c850ea657ad3e29a6878c0203b3e42b4b439
Pavel Makovskiy <snlnrush@gmail.com>
2019-10-25 22:24:41 +0300

    Add Week1 #1

---
+++ Week1/exercise-while-exercise-2.py
@@ -0,0 +1,19 @@
+"""
+Exercise: while exercise 2
+5.0/5.0 points (graded)
+ESTIMATED TIME TO COMPLETE: 5 minutes
+
+2. Convert the following into code that uses a while loop.
+
+prints Hello!
+prints 10
+prints 8
+prints 6
+prints 4
+prints 2
+"""
+loop_var = 10
+print('Hello!')
+while loop_var >= 2:
+    print(loop_var)
+    loop_var -= 2
